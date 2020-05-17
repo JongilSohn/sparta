0adf38d3636d1ec7d6dc43f309aba6d4ac5602c7
JongilSohn <64876056+JongilSohn@users.noreply.github.com>
2020-05-17 20:49:51 +0900

    Java_study

---
+++ Java Study/index.js
@@ -0,0 +1,9 @@
+//alert('I am studing. javascript');  // 새로고침시 팝업 출력.
+console.log('I am studing. javascript');
+
+
+//Variable : 
+//b=2   instruction 
+//a=2   모든 Instruction은 각기 다른 라인에 작성.
+
+//항상 변수를 사용할때는, 1.생성 2. 초기화 3. 사용
